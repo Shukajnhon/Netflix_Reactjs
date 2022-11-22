22af323672bdadcedcdfac2210bc5976a2cc4079
Shukajnhon <87706728+Shukajnhon@users.noreply.github.com>
2022-11-22 16:27:28 +0700

    Add LogOut method by using firebase, built Navbar

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,6 +1,23 @@
 //
+import React, { useState } from "react"
+
+import Navbar from "../components/Navbar"
 function Netflix() {
-    return <h1>Home Netflix</h1>
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    function handlerScroll() {
+        window.onscroll(() => {
+            setIsScrolled(window.pageYOffset === 0 ? false : true)
+            console.log(window.pageYOffset)
+            return () => (window.onscroll = null)
+        })
+
+    }
+
+
+    return <div>
+        <Navbar isScroll={isScrolled} onScroll={handlerScroll} />
+    </div>
 }
 
 export default Netflix
